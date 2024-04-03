f19528e1abbdf8ee17d2289fce808adb76fb91c6
Samson Ameh <amehsam69@gmail.com>
2024-04-03 05:50:55 +0100

    style header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,29 @@
 import styles from "./Header.module.scss";
 import Nav from "./Nav";
+import logo from "@assets/logo.png";
+
+import { HiMenuAlt3 } from "react-icons/hi";
 
 const Header = () => {
-  return <header>Header</header>;
+  return (
+    <header className={styles.header}>
+      <div className={`${styles.container} ${styles.headerContainer}`}>
+        <img className={styles.logo} src={logo} alt="logo" />
+
+        <div className={styles.navContainer}>
+          <Nav />
+        </div>
+
+        <div className={styles.rightCol}>
+          <li className={styles.link}>
+            <a href="#">Contact Us</a>
+          </li>
+
+          <HiMenuAlt3 className={styles.hamburger} />
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
--- src/components/Header/Nav.jsx
+++ src/components/Header/Nav.jsx
@@ -1,7 +1,25 @@
 import styles from "./Nav.module.scss";
+import { Link } from "react-router-dom";
 
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <nav className={styles.nav}>
+      <ul className={styles.linkContainer}>
+        <li className={styles.link}>
+          <a>Home</a>
+        </li>
+        <li className={styles.link}>
+          <a>About Us</a>
+        </li>
+        <li className={styles.link}>
+          <a>Properties</a>
+        </li>
+        <li className={styles.link}>
+          <a>Service</a>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Nav;
